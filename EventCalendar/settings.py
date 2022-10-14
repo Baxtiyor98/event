1140caddf0aaea3408e1a36dbe507e4abee8dfaa
Ergashev Baxtiyor <86454228+Baxtiyor98@users.noreply.github.com>
2022-10-14 15:36:50 +0500

    add

--- EventCalendar/settings.py
+++ EventCalendar/settings.py
@@ -142,5 +142,14 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'doniyorocean@gmail.com'
-EMAIL_HOST_PASSWORD = 'jidwkdimqxmayjdu'
+EMAIL_HOST_USER = 'admin@event.it-center.pro.com'
+EMAIL_HOST_PASSWORD = '!1234567A@admin'
+
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 465
+# EMAIL_USE_TLS = False
+# EMAIL_USE_SSL = True
+# EMAIL_HOST_USER = 'doniyorocean@gmail.com'
+# EMAIL_HOST_PASSWORD = 'jidwkdimqxmayjdu'
